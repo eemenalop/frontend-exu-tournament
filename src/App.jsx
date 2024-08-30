89d075cd057ad30ab7ac362dbff31a7ab046d04b
eemenalop <eemena26@gmail.com>
2024-08-29 22:57:03 -0400

    Se completo el router de las paginas copn el login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from './componets/Home';
 import LogIn from './adminComponets/LogIn';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminHome from './adminComponets/AdminHome';
+import ProtectedRoute from './adminComponets/ProtectedRoute';
 
 
 function App() {
@@ -10,7 +11,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/admin-login' element={<LogIn />} />
-        <Route path='/admin-home' element={<AdminHome />} />
+        <Route path='/admin-home' element={<ProtectedRoute />}>
+          <Route path='/admin-home' element={<AdminHome />} />
+        </Route>
       </Routes>
     </Router>
   );
