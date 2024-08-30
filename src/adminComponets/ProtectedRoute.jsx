90a2a1749a6472394ef6e0b22d696d5424a20de7
eemenalop <eemena26@gmail.com>
2024-08-29 22:58:55 -0400

    Add ProtectedRoute component para protejer la ruta de despues del login

---
+++ src/adminComponets/ProtectedRoute.jsx
@@ -0,0 +1,45 @@
+import { Navigate, Outlet } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { jwtDecode } from "jwt-decode"
+
+function ProtectedRoute() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+
+        if (token) {
+            try {
+                const decoded = jwtDecode(token);
+
+                if (decoded.exp * 1000 > Date.now()) {
+                    setIsAuthenticated(true);
+                } else {
+                    setIsAuthenticated(false);
+                    localStorage.removeItem('token');
+                }
+            } catch (error) {
+                console.log('Error decoding token:', error);
+                setIsAuthenticated(false);
+            }
+        } else {
+            setIsAuthenticated(false);
+        }
+
+        setLoading(false);
+
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (!isAuthenticated) {
+        return <Navigate to='/admin-login' replace />
+    }
+
+    return <Outlet />
+}
+
+export default ProtectedRoute
