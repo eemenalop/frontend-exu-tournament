ed073e8d20027c2aec17c4c149ead23819f2daad
eemenalop <eemena26@gmail.com>
2024-09-02 19:32:46 -0400

    edit logos URL

--- src/teamLogos.js
+++ src/teamLogos.js
@@ -1,6 +1,6 @@
 export const teamLogos = [
-    { id: 1, logo: "https://ulznkxovycorqeblrjxk.supabase.co/storage/v1/object/public/team-logos/nba-atlanta-hawks-logo.png?t=2024-08-29T01%3A46%3A39.066Z" },
-    { id: 2, logo: "https://ulznkxovycorqeblrjxk.supabase.co/storage/v1/object/public/team-logos/nba-boston-celtics-logo.png?t=2024-08-29T01%3A46%3A46.899Z" },
-    { id: 3, logo: "https://ulznkxovycorqeblrjxk.supabase.co/storage/v1/object/public/team-logos/nba-dallas-mavericks-logo.png?t=2024-08-29T01%3A46%3A53.550Z" },
-    { id: 4, logo: "https://ulznkxovycorqeblrjxk.supabase.co/storage/v1/object/public/team-logos/nba-memphis-grizzlies-logo.png?t=2024-08-29T01%3A46%3A59.425Z" },
+    { id: 1, name: 'Popiwa', logo: "https://ulznkxovycorqeblrjxk.supabase.co/storage/v1/object/public/team-logos/popiwa-logo.webp?t=2024-09-02T16%3A18%3A31.360Z" },
+    { id: 2, name: 'Lo Maetro', logo: "https://ulznkxovycorqeblrjxk.supabase.co/storage/v1/object/public/team-logos/lomaetro-logo.webp?t=2024-09-02T16%3A19%3A00.565Z" },
+    { id: 3, name: 'Loudest', logo: "https://ulznkxovycorqeblrjxk.supabase.co/storage/v1/object/public/team-logos/loudest-logo.webp?t=2024-09-02T16%3A18%3A48.223Z" },
+    { id: 4, name: 'Toons', logo: "https://ulznkxovycorqeblrjxk.supabase.co/storage/v1/object/public/team-logos/toons-logo.webp?t=2024-09-02T16%3A19%3A17.811Z" },
 ]
