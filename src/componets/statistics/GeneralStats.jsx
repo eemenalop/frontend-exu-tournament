0bc3e04fc47dbcaf1eaf0235cceaa151c2f801f5
eemenalop <eemena26@gmail.com>
2024-09-04 00:24:00 -0400

    Working on Statics page

--- src/componets/statistics/GeneralStats.jsx
+++ src/componets/statistics/GeneralStats.jsx
@@ -1,17 +1,48 @@
+import { useState } from 'react';
 import Footer from '../Footer';
 import Navbar from '../NavBar';
 import PresentationPage from '../PresentationPage'
-import StatsLeaders from './StatsLeaders'; 
+import StatsLeaders from './StatsLeaders';
+import MatchTypeFilter from '../MatchTypeFilter';
 
 const GeneralStats = () => {
+
+    const [matchType, setMatchType] = useState('Regular');
+    const [statType, setStatType] = useState('points');
 
     return (
         <>
             <Navbar />
             <PresentationPage
                 imageURL='https://ulznkxovycorqeblrjxk.supabase.co/storage/v1/object/public/PresentationPage%20Images/basketball-game-concept.webp?t=2024-09-02T17%3A13%3A10.428Z'
-                alt='Stats component Image'/>
-            <StatsLeaders />
+                alt='Stats component Image' />
+            <div className='px-4 py-6'>
+                <h2 className='text-2xl font-semibold mb-4'>
+                    Top Players
+                </h2>
+                <MatchTypeFilter
+                    selectedMatchType={matchType}
+                    setSelectedMatchType={setMatchType}
+                />
+                <div className='max-w-6xl mx-auto'>
+                    <div className='flex flex-wrap justify-center gap-8'>
+                        <div className='flex flex-wrap justify-center gap-8'>
+                            <StatsLeaders matchType={matchType} statType='points' />
+                            <StatsLeaders matchType={matchType} statType="assists" />
+                            <StatsLeaders matchType={matchType} statType="rebounds" />
+                        </div>
+                        <div className='flex flex-wrap justify-center gap-8'>
+                            <StatsLeaders matchType={matchType} statType="steals" />
+                            <StatsLeaders matchType={matchType} statType="blocks" />
+                            <StatsLeaders matchType={matchType} statType="turnovers" />
+                        </div>
+                        <div className='flex flex-wrap justify-center gap-8'>
+                            <StatsLeaders matchType={matchType} statType="threeptm" />
+                            <StatsLeaders matchType={matchType} statType="pra" />
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </>
     );
--- src/componets/statistics/StatsLeaders.jsx
+++ src/componets/statistics/StatsLeaders.jsx
@@ -1,11 +1,58 @@
+import React, { useEffect, useState } from 'react';
 
+const StatsLeaders = ({ matchType, statType }) => {
+    const [stats, setStats] = useState([]);
 
-const StatsLeaders = () => {
+    useEffect(() => {
+        const fetchStats = async () => {
+            try {
+                const response = await fetch(
+                    `http://localhost:4000/.netlify/functions/getTopPlayerStats?match_type=${matchType}&stat_type=${statType}`
+                );
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+
+                setStats(data);
+
+            } catch (error) {
+                console.error('Error fetching stats:', error);
+            }
+        };
+
+        fetchStats();
+    }, [matchType, statType]);
 
     return (
-        <>
-            <h2>Lideres del torneo</h2>
-        </>
+        <div className="my-6 p-4 bg-white shadow-md rounded-lg">
+            <h3 className="text-2xl font-semibold text-gray-800 mb-4 border-b border-gray-300 pb-2">
+                {statType.toUpperCase()}
+            </h3>
+            <ul className="space-y-3">
+                {stats.map((player, index) => (
+                    <li
+                        key={index}
+                        className="flex items-center justify-between p-3 bg-gray-100 rounded-lg shadow-sm hover:bg-gray-200 transition duration-300 ease-in-out"
+                    >
+                        <span className="w-6 h-6 flex items-center justify-center text-sm font-semibold text-white bg-blue-500 rounded-full mr-4">
+                            {player.rank}
+                        </span>
+                        <div className="flex-grow text-center">
+                            <p className="text-lg font-medium text-gray-900">
+                                {player.player_name}
+                            </p>
+                            <p className="text-sm text-gray-600">
+                                {player.team_name}
+                            </p>
+                        </div>
+                        <p className="text-lg font-semibold text-gray-900">
+                            {player.stat_per_game}
+                        </p>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
